495767bcc4943d341facc16b15b6d267d70cfa14
mrmsdev <s.mbr@hotmail.fr>
2026-05-30 00:14:46 +0100

    fix

--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -1,7 +1,6 @@
 import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../../lib/prisma-client/client'
-
+import { PrismaClient } from "@prisma/client"
 const connectionString = process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5432/phonerepair?schema=public'
 
 const pool = new Pool({ connectionString })
